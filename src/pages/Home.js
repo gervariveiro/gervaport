6a94856a108d515ed96c7310940533812338e149
Gerva Riveiro <84820639+gervariveiro@users.noreply.github.com>
2022-09-23 13:42:39 -0300

    section 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -50,7 +50,7 @@ function Home () {
                             modi ipsam asperiores molestias, ut qui repudiandae architecto dolore quibusdam labore 
                             exercitationem. Reprehenderit natus modi nesciunt aperiam repellat?</p>
                         <p className="h4 pb-2">- Gervasio Riveiro</p>
-                        <Button variant="secondary" size="lg" href="https://f1backend.herokuapp.com/downloadcv">
+                        <Button variant="light" size="lg" href="https://f1backend.herokuapp.com/downloadcv">
                             My Resume
                             <FontAwesomeIcon icon={faFileArrowDown} className='downloadIcon' />
                         </Button>
@@ -90,64 +90,94 @@ function Home () {
         
         <section>
             <h3 className="m-5">My Skills</h3>
-            <Container>
-                <Row className='skillcontainer'>
-                    <Col>
+            <Container className='d-flex flex-row justify-content-md-adround'>
+                <Row >
+                    <Col col md={6}>
                         <ul className='skill'>
-                            <li>
+                            <li className='mb-4'>
                                 <img src={HtmlIcon} width="50" height="50"></img>
                                 HTML
                             </li>
-                            <li>
+                            <li className='mb-4'>
                                 <img src={CssIcon} width="50" height="50"></img>
                                 CSS
                             </li>
-                            <li>
+                            <li className='mb-4'>
                                 <img src={DbIcon} width="50" height="50"></img>
                                 MONGDB-MYSQL
                             </li>
-                            <li>
-                                <img src={NodeIcon} width="50" height="50"></img>
-                                NODEJS
-                            </li>
-                            <li>
-                                <img src={JsIcon} width="50" height="50"></img>
-                                JAVASCRIPT
-                            </li>
-                            <li>
-                                <img src={ReactIcon} width="50" height="50"></img>
-                                React
+                            <li className='mb-4'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#042C4B" class="bi bi-bootstrap-fill" viewBox="0 0 16 16">
+                                    <path d="M6.375 7.125V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z"/>
+                                    <path d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12V3.545h3.399c1.587 0 2.543.809 2.543 2.11 0 .884-.65 1.675-1.483 1.816v.1c1.143.117 1.904.931 1.904 2.033 0 1.488-1.084 2.396-2.888 2.396H5.062z"/>
+                                </svg>
+                                Bootstrap
                             </li>
                         </ul>
                     </Col>
-                   
+                </Row>
+                <Row>
+                    <Col col md={6}>
+                        <ul className='skill'>
+                            <li className='mb-4'>
+                                <img src={NodeIcon} width="50" height="50"></img>
+                                NODEJS
+                            </li>
+                            <li className='mb-4'>
+                                <img src={JsIcon} width="50" height="50"></img>
+                                JAVASCRIPT
+                            </li>
+                            <li className='mb-4'>
+                                <img src={ReactIcon} width="50" height="50"></img>
+                                React
+                            </li>
+                            
+                        </ul>
+                    </Col>
                 </Row>
             </Container>
             
         </section>
-
-        <h2 className='title2' id='talk'>Let`s Talk</h2><ul className='redes'>
-                <li className=''>
-                    <a href='https://www.linkedin.com/in/gervasio-riveiro-martínez-36a8b0234/' target="_blank">
-                        <img src={LinkedIcon} className='imgicon'></img>
-                    </a>
-                </li>
-                <li className=''>
-                    <a href='https://www.facebook.com/gervasio.riveiro/' target="_blank">
-                        <img src={FaceIcon} className='imgicon'></img>
-                    </a>
-                </li>
-                <li className=''>
-                    <a href='https://github.com/gervariveiro' target="_blank">
-                        <img src={GitHubIcon} className='imgicon'></img>
-                    </a>
-                </li>
-                <li className=''>
-                    <a href='https://www.twitch.tv/rashgaminguy/' target="_blank">
-                        <img src={TwitchIcon} className='imgicon'></img>
-                    </a>
-                </li>
-            </ul><form className='formcontact' action="https://f1backend.herokuapp.com/send-emailportfolio" method="POST">
+        <section>
+            <h3 className="m-5">Contact Me</h3>
+            <Container>
+                <Row>
+                    <Col>
+                        <ul className='redes'>
+                            <li className=''>
+                            <a href='https://www.linkedin.com/in/gervasio-riveiro-martínez-36a8b0234/' target="_blank">
+                                <img src={LinkedIcon} className='imgicon'></img>
+                            </a>
+                        </li>
+                        <li className=''>
+                            <a href='https://www.facebook.com/gervasio.riveiro/' target="_blank">
+                                <img src={FaceIcon} className='imgicon'></img>
+                            </a>
+                        </li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ul>
+                            <li className=''>
+                                <a href='https://github.com/gervariveiro' target="_blank">
+                                    <img src={GitHubIcon} className='imgicon'></img>
+                                </a>
+                            </li>
+                            <li className=''>
+                                <a href='https://www.twitch.tv/rashgaminguy/' target="_blank">
+                                    <img src={TwitchIcon} className='imgicon'></img>
+                                </a>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+                    
+            </Container>
+        </section>
+                
+                <form className='formcontact' action="https://f1backend.herokuapp.com/send-emailportfolio" method="POST">
                 <label>
                     Name:
                     <input type="text" name="name" placeholder='Name' />
