1c1207fe67b973372d4c1de4dbdf82d6ea1e1b48
Gerva Riveiro <84820639+gervariveiro@users.noreply.github.com>
2022-09-26 17:28:10 -0300

    form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,10 +46,9 @@ function Home () {
                     </Col>
                     <Col className="col-12 text-center">
                         <h3 className="mb-3"> About Me</h3>
-                        <p className="h5 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum corporis fugit 
-                            modi ipsam asperiores molestias, ut qui repudiandae architecto dolore quibusdam labore 
-                            exercitationem. Reprehenderit natus modi nesciunt aperiam repellat?</p>
-                        <p className="h4 pb-2">- Gervasio Riveiro</p>
+                        <p className="h5 mb-4">Hi how are you?
+                        On this site you can see some of my work. I offer services in dynamic web site development for your business or tool for work with data.</p>
+                        <p className="h4 pb-2">Gervasio Riveiro</p>
                         <Button variant="light" size="lg" href="https://f1backend.herokuapp.com/downloadcv">
                             My Resume
                             <FontAwesomeIcon icon={faFileArrowDown} className='downloadIcon' />
@@ -107,7 +106,7 @@ function Home () {
                                 MONGDB-MYSQL
                             </li>
                             <li className='mb-4'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#042C4B" class="bi bi-bootstrap-fill" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#042C4B" className="bi bi-bootstrap-fill" viewBox="0 0 16 16">
                                     <path d="M6.375 7.125V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z"/>
                                     <path d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12V3.545h3.399c1.587 0 2.543.809 2.543 2.11 0 .884-.65 1.675-1.483 1.816v.1c1.143.117 1.904.931 1.904 2.033 0 1.488-1.084 2.396-2.888 2.396H5.062z"/>
                                 </svg>
@@ -180,39 +179,29 @@ function Home () {
                 </Row>
                     
             </Container>
-            <Form className="mx-5">
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+            </section>
+            <section>
+            <Form widht="250px" className="mx-5 formclass" action="https://f1backend.herokuapp.com/send-emailportfolio" method='POST'>
+                <Form.Group className="mb-3">
                     <h5>Name</h5>
-                    <Form.Control class="in-line my-2 mx-4" type="email" placeholder="Enter email" />
+                    <Form.Control className="my-2 mx-4" placeholder="Enter name" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <h5>email</h5>
-                    <Form.Control class="in-line my-2 mx-4" type="email" placeholder="Enter email" />
+                    <h5>Email</h5>
+                    <Form.Control className="my-2 mx-4"  type="email" placeholder="Enter email" />
                 </Form.Group>
-                <Form.Text id="passwordHelpBlock" placeholder="Write a message">
-                    
-                </Form.Text>
-                <Button variant="primary" type="submit">
-                    Submit
+                <Form.Group as={Col} id="formGridQuery">
+                <h5>Message</h5>
+                <Form.Control className="m-4 textarea" name="query" as="textarea" rows={3} />
+                </Form.Group>
+                <Button variant="primary" className='mx-4 my-3' type="submit">
+                    Send
                 </Button>
         </Form>
         </section>
+        
                 
-                <form className='formcontact' action="https://f1backend.herokuapp.com/send-emailportfolio" method="POST">
-                <label>
-                    Name:
-                    <input type="text" name="name" placeholder='Name' />
-                </label>
-                <label>
-                    Email:
-                    <input type="text" name="email" placeholder='Email' />
-                </label>
-                <label className='labelMessage'>
-                    Menssage:
-                    <textarea className='mensaje' type="text" name="message" placeholder='Write your message' />
-                </label>
-                <button className='buttonsend' type="submit" value="Enviar" onClick={() => mostrarAlert()}>Send</button>
-            </form></>
+        </>
                 
 
     )
